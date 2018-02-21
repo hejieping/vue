cc27ef1fcb405d51e1471bde9553f6410eb6db93
何杰平 <hejieping4399@qq.com>
2018-02-21 11:30:25 +0800

    解构赋值

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -6,6 +6,37 @@
 </template>
 
 <script>
+//值交换
+let x = 1;
+let y = 2;
+[x,y] = [y,x];
+console.log(x,y);
+//从函数返回多个值
+function example1(){
+  return [1,2,3];
+}
+let [a,b,c] = example1();
+console.log(a,b,c);
+//函数参数的定义
+function example2({x, y, z}){
+  console.log(x,y,z);
+}
+example2({z:5,x:6,y:7});
+//提取json数据
+let jsonData = {
+  id:42,
+  status:"ok",
+  data:[123,512]
+}
+let {id,status,data:number} = jsonData;
+console.log(id,status,number);
+//遍历map数据
+const  map = new Map();
+map.set("first","one");
+map.set("second","two");
+for(let [key,value] of map){
+ console.log(key + " is " + value);
+}
 export default {
   name: 'App'
 }
